7d256a3f7e55d1338a6df2d70886a8484940b4cd
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2020-12-18 22:56:07 +0530

    development is done

---
+++ app.js
@@ -0,0 +1,31 @@
+var input = document.querySelector(".input");
+var button = document.querySelector(".btn");
+var output = document.querySelector(".output");
+
+var service = "https://api.funtranslations.com/translate/emoji.json";
+
+
+button.addEventListener("click", clickHandler)
+
+function clickHandler() {
+    
+    
+    fetch(fullApi(input.value)).then(response=>response.json()).then(json=>{
+        console.log(json);
+        output.innerText = json.contents.translated;
+        
+    });
+
+
+}
+
+function fullApi(text) {
+
+    var url = service + "?" + "text=" + text;
+
+    console.log(url);
+
+    return url;
+}
+
+
